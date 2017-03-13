2cf162bb6679f46da795d59a6b8984e25f0cd9d5
andy liang <aliang2344@gmail.com>
2017-03-12 20:50:09 -0400

    reconciling having to have a separate merge_sort and chunk_sort

--- src/external_sort/mod.rs
+++ src/external_sort/mod.rs
@@ -9,7 +9,7 @@ pub trait Serializable {
 }
 
 pub trait EventLike {
-    fn get_event_ame(&self) -> &str;
+    fn get_event_name(&self) -> &str;
     fn get_unique_id(&self) -> &str;
     fn get_time(&self) -> &u64;
 }
@@ -28,28 +28,28 @@ pub struct SortOptions {
 
 use std::fs::File;
 
-struct FileIterator<I> {
+struct FileIterator<'a, I: 'a> {
     inner: Box<Iterator<Item=I>>,
     options: SortOptions,
     num: usize,
-    sort_by: Box<Fn(&I, &I) -> Ordering>
+    sort_by: &'a Fn(&I, &I) -> Ordering
 }
 
-impl <I> FileIterator<I> {
-    fn new<F>(inner: Box<Iterator<Item=I>>, options: SortOptions, sort_fn: F) -> FileIterator<I>
-        where F: Fn(&I, &I) -> Ordering + 'static {
+impl <'a, I> FileIterator<'a, I> {
+    fn new<F>(inner: Box<Iterator<Item=I>>, options: SortOptions, sort_fn: &'a F) -> FileIterator<'a, I>
+        where F: Fn(&I, &I) -> Ordering + 'a {
         FileIterator {
             inner: inner,
             options: options,
             num: 0,
-            sort_by: Box::new(sort_fn)
+            sort_by: sort_fn
         }
     }
 }
 
 // iterating over open files seems dangerous maybe? if the iterator is unspooled into a collection
 // this could get messy.
-impl <I> Iterator for FileIterator<I> where I: Serializable {
+impl <'a, I> Iterator for FileIterator<'a, I> where I: Serializable {
     type Item = File;
     fn next(&mut self) -> Option<Self::Item> {
         use std::io::Write;
@@ -83,23 +83,24 @@ pub struct ExternalSort<I: Clone> {
 }
 
 impl <I> ExternalSort<I> where I: Clone + Serializable {
-    fn new<F>(to_sort: Box<Iterator<Item=I>>, options: SortOptions, chunk_sort: F, merge_sort: F)
+    fn new<F>(to_sort: Box<Iterator<Item=I>>, options: SortOptions, chunk_sort: F)
         -> ExternalSort<I> where F: Fn(&I, &I) -> Ordering + 'static {
         use std::io::{BufRead, BufReader};
         use std::io::Lines;
 
-        let files = FileIterator::new(to_sort, options, chunk_sort);
-
-        let sorted_chunks = files.map(|file| {
-            let p = BufReader::new(file)
-                .lines()
-                .map(|line| I::deserialize(line.unwrap().as_bytes()));
-            IHead::new(p)
-        }).collect::<Vec<_>>();
+        let sorted_chunks = {
+            let files = FileIterator::new(to_sort, options, &chunk_sort);
+            files.map(|file| {
+                let p = BufReader::new(file)
+                    .lines()
+                    .map(|line| I::deserialize(line.unwrap().as_bytes()));
+                IHead::new(p)
+            }).collect::<Vec<_>>()
+        };
 
         ExternalSort {
             sorted_chunks: sorted_chunks,
-            sort_fn: Box::new(merge_sort)
+            sort_fn: Box::new(chunk_sort)
         }
     }
 }
@@ -138,7 +139,6 @@ struct IHead<I> {
 impl <I> IHead<I> where I: Clone {
     fn new<A>(mut iterator: A) -> IHead<I> where A: Iterator<Item=I> + 'static {
         let head = iterator.next();
-
         IHead {
             iterator: Box::new(iterator),
             head: head
@@ -153,4 +153,3 @@ impl <I> IHead<I> where I: Clone {
         head
     }
 }
-
